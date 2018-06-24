8c676c316132b363ad971757888176990c5aaee4
Dan Kogai <dankogai+github@gmail.com>
2018-06-24 21:05:25 +0900

    borrow fastDividingFullWidth from attaswift/BigInt

--- Sources/Int2XRun/main.swift
+++ Sources/Int2XRun/main.swift
@@ -1,9 +1,33 @@
 import Int2X
 
-typealias U2048 = UInt2X<UInt1024>
-
 func fact<T:FixedWidthInteger>(_ n:T)->T {
-    return n == 0 ? 1 : (1...Int(n)).map{ T($0) }.reduce(1){ print($1) ; return $0 * $1 }
+    return n == 0 ? 1 : (1...Int(n)).map{ T($0) }.reduce(1, *)
 }
 
-print(fact(U2048(300)))
+//for flag in [true, false] {
+//    Int2XConfig.useAccelerate = flag
+//    let v = fact(Int512(97)) / fact(Int512(56))
+//    print(v)
+//}
+
+for flag in [true, false] {
+    Int2XConfig.useAccelerate = flag
+    let v = fact(Int256(56)) / fact(Int256(21))
+    print(v)
+}
+
+
+////print(fact(U2048(300)))
+
+//Int2XConfig.useAccelerate = false
+//let imax = 56
+//let factmax = fact(Int256(imax))
+//var dummy = factmax
+//print(factmax)
+//for _ in (0..<1) {
+//    for i in (0 ... imax) {
+//        dummy = (factmax / fact(Int256(i)))
+//        print(i, dummy)
+//    }
+//}
+//print(dummy)
--- macOS.playground/Pages/ScratchInt2X.xcplaygroundpage/Contents.swift
+++ macOS.playground/Pages/ScratchInt2X.xcplaygroundpage/Contents.swift
@@ -25,7 +25,7 @@ I16(-42).quotientAndRemainder(dividingBy: I16(-5)).remainder
 (I16(0)..<I16(8))[4]
 
 I16.min == I16(0)
-
+I16.min == 0 // wrongly true
 Int128.min.toString(radix:16)
 Int256.min.toString(radix:16)
 Int512.min.toString(radix:16)
@@ -34,7 +34,7 @@ func fact<T:FixedWidthInteger>(_ n:T)->T {
     return n == 0 ? 1 : (1...Int(n)).map{ T($0) }.reduce(1, *)
 }
 
-fact(Int128(34))
+fact(Int128(34)) // wrongly negative
 fact(Int256(34))
 var i256:Int256 = "-0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
 
