d8a8effbce7aedc5de5bbd9b8201b660f6cc4eb7
Dan Kogai <dankogai+github@gmail.com>
2022-05-10 14:40:03 +0900

    remove "type: .dynamic"

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,7 +9,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Int2X",
-            type: .dynamic,
             targets: ["Int2X"]),
     ],
     dependencies: [
